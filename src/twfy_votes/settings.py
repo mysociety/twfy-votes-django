1f581407e4f6174cf198cd02f96d739e670abf65
Sam Pearson <sam@sgp.me.uk>
2024-11-05 10:30:12 +0000

    Get settings from environment

--- src/twfy_votes/settings.py
+++ src/twfy_votes/settings.py
@@ -29,17 +29,13 @@ environ.Env.read_env(BASE_DIR / ".env")
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / ".static"
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ka$edl#zy75+_@ohr-s7kt6m_%#j501c!9k_ofcu&77k*5ur3a"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+SECRET_KEY = env("SECRET_KEY")
+DEBUG = env("DEBUG")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS")
+CACHE_FILE = env("CACHE_FILE")
+HIDE_DEBUG_TOOLBAR = env("HIDE_DEBUG_TOOLBAR")
+GOOGLE_ANALYTICS = env("GOOGLE_ANALYTICS")
+TWFY_API_KEY = env("TWFY_API_KEY")
 
 # Application definition
 
