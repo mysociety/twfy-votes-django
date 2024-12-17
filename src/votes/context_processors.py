6fd03eef951243cea5bd16f8a0daa07a59591aa0
Alex Parsons <alex.parsons@mysociety.org>
2024-12-17 21:41:04 +0000

    More general feature flag support

--- src/votes/context_processors.py
+++ src/votes/context_processors.py
@@ -6,4 +6,5 @@ from .views.auth import can_view_draft_content
 def extra_context(request: HttpRequest) -> dict:
     return {
         "can_view_draft_content": can_view_draft_content(request.user),
+        "request": request,
     }
