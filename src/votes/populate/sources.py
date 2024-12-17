5a3024fbefd45005d01bc750709ea557726bf29f
Alex Parsons <alex.parsons@mysociety.org>
2024-12-17 15:14:29 +0000

    Consistent approach to pre 2010 votes
    - Remove some from draft policies
    - Add others to legacy import when the gid is invalid in twfy

--- src/votes/populate/sources.py
+++ src/votes/populate/sources.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from django.conf import settings
 
 import httpx
+import pandas as pd
 import rich
 
 from .register import ImportOrder, import_register
@@ -32,3 +33,18 @@ def get_external_data(quiet: bool = False):
             with target.open("wb") as f:
                 for chunk in response.iter_bytes():
                     f.write(chunk)
+
+    # patches divisions file
+    # we're missing some old relevant policy ids because the gid id is missing in
+    # the twfy hansard table
+
+    legacy_df = pd.read_json(Path(BASE_DIR, "data", "lookups", "legacy_divisions.json"))
+
+    date_columns = ["division_date", "lastupdate"]
+
+    for d in date_columns:
+        legacy_df[d] = pd.to_datetime(legacy_df[d])
+
+    df = pd.read_parquet(source_dir / "divisions.parquet")
+    df = pd.concat([df, legacy_df], ignore_index=True)
+    df.to_parquet(source_dir / "divisions.parquet", index=False)
