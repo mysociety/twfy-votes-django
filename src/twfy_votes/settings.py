969d62fff7a859b722b3cd4b988f4e89fa200251
Alex Parsons <alex.parsons@mysociety.org>
2024-12-17 20:31:57 +0000

    Add draft content system
    - Not meant to be high security, just a basic public concealing of some features

--- src/twfy_votes/settings.py
+++ src/twfy_votes/settings.py
@@ -80,6 +80,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "votes.context_processors.extra_context",
             ],
             "builtins": [
                 "votes.templatetags.hub_filters",
---
+++ src/votes/populate/user_groups.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import Group
+
+from .register import ImportOrder, import_register
+
+
+@import_register.register("user_groups", group=ImportOrder.USER_GROUPS)
+def add_user_groups(quiet: bool = False):
+    groups_to_create = ["access_in_progress"]
+
+    for g in groups_to_create:
+        Group.objects.get_or_create(name=g)
