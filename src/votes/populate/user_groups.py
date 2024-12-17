1288776a0bc42466d637289e35d39af45445afdc
Alex Parsons <alex.parsons@mysociety.org>
2024-12-17 21:39:21 +0000

    add advanced_info flag

--- src/votes/populate/user_groups.py
+++ src/votes/populate/user_groups.py
@@ -5,7 +5,7 @@ from .register import ImportOrder, import_register
 
 @import_register.register("user_groups", group=ImportOrder.USER_GROUPS)
 def add_user_groups(quiet: bool = False):
-    groups_to_create = ["access_in_progress"]
+    groups_to_create = ["access_in_progress", "advanced_info"]
 
     for g in groups_to_create:
         Group.objects.get_or_create(name=g)
