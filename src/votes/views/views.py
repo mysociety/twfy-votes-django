cd7d1af6a7f4a6b4da89eb7499a647b3353fecaa
Alex Parsons <alex.parsons@mysociety.org>
2024-12-24 10:56:49 +0000

    Add decision list menu

--- src/votes/views/views.py
+++ src/votes/views/views.py
@@ -344,7 +344,7 @@ class DecisionsPageView(TitleMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["chambers"] = Chamber.objects.all()
+        context["chambers"] = Chamber.objects.all().exclude(slug="ni")
         return context
 
 
