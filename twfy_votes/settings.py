548951970d2d10dc2c24ca40a0c34bffc8502167
Alex Parsons <alex.parsons@mysociety.org>
2025-02-11 13:23:23 +0000

    Add CSRF_TRUSTED_ORIGINS for live

--- twfy_votes/settings.py
+++ twfy_votes/settings.py
@@ -71,6 +71,8 @@ if DEBUG and HIDE_DEBUG_TOOLBAR is False:  # pragma: no cover
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS = [ip[:-1] + "1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
     CSRF_TRUSTED_ORIGINS = ["https://*.preview.app.github.dev"]
+else:
+    CSRF_TRUSTED_ORIGINS = ["https://votes.theyworkforyou.com"]
 
 ROOT_URLCONF = "twfy_votes.urls"
 
