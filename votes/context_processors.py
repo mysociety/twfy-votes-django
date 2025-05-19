4b1d8e99fb64be21df6816d6722ae48986919107
Alex Parsons <alex.parsons@mysociety.org>
2025-05-19 08:33:42 +0000

    Add analytics key to global context

--- votes/context_processors.py
+++ votes/context_processors.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.http import HttpRequest
 
 from .consts import PermissionGroupSlug
@@ -9,4 +10,5 @@ def extra_context(request: HttpRequest) -> dict:
         "can_view_draft_content": can_view_draft_content(request.user),
         "request": request,
         "permission_group": PermissionGroupSlug,
+        "GOOGLE_ANALYTICS": settings.GOOGLE_ANALYTICS,
     }
